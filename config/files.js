531c209bd6b05f4da8ac02c58bf56251e913e9fe
Alex Chien <alex.chien@koocaa.com>
2014-11-25 03:03:24 +0800

    using bower to manage front-end dependencies

--- config/files.js
+++ config/files.js
@@ -10,34 +10,58 @@ module.exports = function(lineman) {
   return {
     js: {
       vendor: [
-        "vendor/js/jquery.js",
-        "vendor/js/jquery.growl.js",
-        // "vendor/js/ark.js",
-        "vendor/js/stacktrace.js",
-        // "vendor/js/angular.js",
-
+        "vendor/bower/jquery/dist/jquery.js",
+        "vendor/bower/growl/javascripts/jquery.growl.js",
+        "vendor/bower/stacktrace/dist/stacktrace.js",
+        "vendor/bower/angular/angular.js",
+        "vendor/js/bootstrap.js",
         // angular-material
-        "bower_components/angular/angular.js",
-        "bower_components/angular-aria/angular-aria.js",
-        "bower_components/angular-animate/angular-animate.js",
-        "bower_components/hammerjs/hammer.js",
-        "bower_components/angular-material/angular-material.js",
-        "bower_components/angular-aria/angular-aria.js",
-        "bower_components/angular-animate/angular-animate.js",
+        "vendor/bower/angular-animate/angular-animate.js",
+        "vendor/bower/angular-aria/angular-aria.js",
+        "vendor/bower/hammerjs/hammer.js",
+        "vendor/bower/angular-material/angular-material.js",
 
-        "vendor/js/angular-resource.js",
+        "vendor/bower/angular-resource/angular-resource.js",
+        // "vendor/bower/angular-ui-router/angular-ui-router.js",
         "vendor/js/angular-ui-router.js",
-        "vendor/js/ui-bootstrap-tpls.js",
-         "vendor/js/ui-grid.js",
-        "vendor/js/angular-idle.js",
-        "vendor/js/validate.js",
-        "vendor/js/xeditable.js",
-        "vendor/js/angular-translate.min.js",
-        "vendor/js/angular-translate-loader-static-files.min.js",
+        "vendor/bower/angular-translate/angular-translate.js",
+        "vendor/bower/angular-translate-loader-static-files/angular-translate-loader-static-files.js",
+
+        "vendor/bower/angular-pageslide-directive/dist/angular-pageslide-directive.js",
+        "vendor/bower/ng-idle/angular-idle.js",
+        "vendor/bower/angular-bootstrap/ui-bootstrap.js",
+        "vendor/bower/angular-bootstrap/ui-bootstrap-tpls.js",
+        "vendor/bower/angular-ui-grid/ui-grid.js",
+        "vendor/bower/angular-ui-utils/validate.js",
+        "vendor/bower/angular-xeditable/dist/js/xeditable.js",
+        "vendor/bower/moment/min/moment-with-locales.js",
         "vendor/js/jsonpath.js",
-        "vendor/js/angular-pageslide-directive.js",
         "vendor/js/highstock.src.js",
-        "vendor/js/moment-with-locales.js"
+
+
+    // version conflicts to solve
+    // "angular-pageslide-directive": "~0.1.8",
+    // "ng-idle": "~0.3.5"
+
+
+        // "vendor/js/jquery.js",
+        // "vendor/js/jquery.growl.js",
+        // // "vendor/js/ark.js",
+        // "vendor/js/stacktrace.js",
+        // "vendor/js/angular.js",
+        // "vendor/js/angular-resource.js",
+        // "vendor/js/angular-ui-router.js",
+        // "vendor/js/ui-bootstrap-tpls.js",
+        // "vendor/js/ui-grid.js",
+        // "vendor/js/angular-idle.js",
+        // "vendor/js/validate.js",
+        // "vendor/js/xeditable.js",
+        // "vendor/js/angular-translate.min.js",
+        // "vendor/js/angular-translate-loader-static-files.min.js",
+        // "vendor/js/jsonpath.js",
+        // "vendor/js/angular-pageslide-directive.js",
+        // "vendor/js/highstock.src.js",
+        // "vendor/js/moment-with-locales.js"
       ],
       app: [
         "app/js/app.js",
@@ -54,30 +78,39 @@ module.exports = function(lineman) {
 //    },
 
     css: {
-      // vendor: [
-      //   "vendor/css/jquery.growl.css",
-      //   "vendor/css/bootstrap.css",
-      //   "vendor/css/font-awesome.css",
-      //   "vendor/css/ark.css",
-      //   "vendor/css/xeditable.css",
-      //   "vendor/css/ui-grid.css",
-      //   "vendor/css/material.css",
-      //   "vendor/css/material-wfont.css",
-      //   "vendor/css/ripples.css",
-      //   "bower_components/angular-material/angular-material.css",
-      // ],
+      vendor: [
+        "vendor/bower/growl/jquery.growl.css",
+        "vendor/bower/angular-material/angular-material.css",
+        "vendor/bower/angular-ui-grid/ui-grid.css",
+        "vendor/bower/angular-xeditable/dist/css/xeditable.css",
+        "vendor/css/bootstrap.css",
+        "vendor/css/font-awesome.css",
+        "vendor/css/ark.css"
+
+
+        // "vendor/css/jquery.growl.css",
+        // "vendor/css/bootstrap.css",
+        // "vendor/css/font-awesome.css",
+        // "vendor/css/ark.css",
+        // "vendor/css/xeditable.css",
+        // "vendor/css/ui-grid.css"
+      ],
       app: [
-      //   "app/css/bootstrap_overrides.css",
-      //   "app/css/main.css",
-      //   "app/css/forms.css",
-      //   "app/css/layout.css",
-      //   "app/css/my-ng-grid.css",
-      //   "app/css/toolbar.css",
-      //   "app/css/footer.css",
-      //   "app/css/market.css",
-      //   "app/css/spinner.css",
-      //   "app/css/help.css",
-        "app/css/splashpage.scss"
+        "app/css/*.css",
+        "app/css/main.scss"
+
+        // "app/css/bootstrap_overrides.css",
+        // "app/css/main.css",
+        // "app/css/forms.css",
+        // "app/css/layout.css",
+        // "app/css/my-ng-grid.css",
+        // "app/css/toolbar.css",
+        // "app/css/footer.css",
+        // "app/css/market.css",
+        // "app/css/spinner.css",
+        // "app/css/help.css",
+        //
+        // "app/main.scss"
       ]
     }
 
