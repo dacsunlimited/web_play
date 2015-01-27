cceef64ed5a48e89c4378ebd03867107d7735921
Alex Chien <alex.chien@koocaa.com>
2015-01-27 16:30:04 +0800

    house keeping

--- config/files.js
+++ config/files.js
@@ -23,7 +23,6 @@ module.exports = function(lineman) {
         "vendor/bower/angular-material/angular-material.js",
 
         "vendor/bower/angular-resource/angular-resource.js",
-        // "vendor/bower/angular-ui-router/angular-ui-router.js",
         "vendor/js/angular-ui-router.js",
         "vendor/bower/angular-translate/angular-translate.js",
         "vendor/bower/angular-translate-loader-static-files/angular-translate-loader-static-files.js",
@@ -36,41 +35,8 @@ module.exports = function(lineman) {
         "vendor/bower/angular-ui-utils/validate.js",
         "vendor/bower/angular-xeditable/dist/js/xeditable.js",
         "vendor/bower/moment/min/moment-with-locales.js",
-        // "vendor/js/ui-bootstrap-tpls.js",
-        //  "vendor/js/ui-grid.js",
-        // "vendor/js/angular-idle.js",
-        // "vendor/js/validate.js",
-        // "vendor/js/xeditable.js",
-        // "vendor/js/angular-translate.min.js",
-        // "vendor/js/angular-translate-loader-static-files.min.js",
         "vendor/js/jsonpath.js",
         "vendor/js/highstock.src.js",
-
-
-    // version conflicts to solve
-    // "angular-pageslide-directive": "~0.1.8",
-    // "ng-idle": "~0.3.5"
-
-
-        // "vendor/js/jquery.js",
-        // "vendor/js/jquery.growl.js",
-        // // "vendor/js/ark.js",
-        // "vendor/js/stacktrace.js",
-        // "vendor/js/angular.js",
-        // "vendor/js/angular-resource.js",
-// "vendor/js/angular-idle.js",
-        // "vendor/js/angular-ui-router.js",
-        // "vendor/js/ui-bootstrap-tpls.js",
-        // "vendor/js/ui-grid.js",
-        // "vendor/js/angular-idle.js",
-        // "vendor/js/validate.js",
-        // "vendor/js/xeditable.js",
-        // "vendor/js/angular-translate.min.js",
-        // "vendor/js/angular-translate-loader-static-files.min.js",
-        // "vendor/js/jsonpath.js",
-        // "vendor/js/angular-pageslide-directive.js",
-        // "vendor/js/highstock.src.js",
-        // "vendor/js/moment-with-locales.js"
         "vendor/js/moment-with-locales.js",
         "vendor/js/bytebuffer.js"
       ],
@@ -97,58 +63,11 @@ module.exports = function(lineman) {
         "vendor/css/bootstrap.css",
         "vendor/css/font-awesome.css",
         "vendor/css/ark.css"
-
-
-        // "vendor/css/jquery.growl.css",
-        // "vendor/css/bootstrap.css",
-        // "vendor/css/font-awesome.css",
-        // "vendor/css/ark.css",
-        // "vendor/css/xeditable.css",
-        // "vendor/css/ui-grid.css"
       ],
       app: [
         "app/css/*.css",
         "app/css/main.scss"
-
-        // "app/css/bootstrap_overrides.css",
-        // "app/css/main.css",
-        // "app/css/forms.css",
-        // "app/css/layout.css",
-        // "app/css/my-ng-grid.css",
-        // "app/css/toolbar.css",
-        // "app/css/footer.css",
-        // "app/css/market.css",
-        // "app/css/spinner.css",
-        // "app/css/mail.css",
-        // "app/css/help.css",
-        // "app/css/splashpage.css",
-        //
-        // "app/main.scss"
       ]
-      // vendor: [
-      //   "vendor/css/jquery.growl.css",
-      //   "vendor/css/bootstrap.css",
-      //   "vendor/css/font-awesome.css",
-      //   "vendor/css/ark.css",
-      //   "vendor/css/xeditable.css",
-      //   "vendor/css/ui-grid.css",
-      //   "vendor/css/material.css",
-      //   "vendor/css/material-wfont.css",
-      //   "vendor/css/ripples.css",
-      // ],
-      // app: [
-      //   "app/css/bootstrap_overrides.css",
-      //   "app/css/main.css",
-      //   "app/css/forms.css",
-      //   "app/css/layout.css",
-      //   "app/css/my-ng-grid.css",
-      //   "app/css/toolbar.css",
-      //   "app/css/footer.css",
-      //   "app/css/market.css",
-      //   "app/css/spinner.css",
-      //   "app/css/help.css",
-      //   "app/css/splashpage.scss"
-      // ]
     }
 
   };
